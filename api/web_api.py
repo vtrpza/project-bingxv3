1b837f4cae39c027333918bf5b00a2b4c185c44b
Vitor <vhnpouza@gmail.com>
2025-07-24 07:47:42 -0300

    1

--- api/web_api.py
+++ api/web_api.py
@@ -875,7 +875,8 @@ mount_frontend()
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    # Render's default port is 10000
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(
         app,
         host="0.0.0.0",
